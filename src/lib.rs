1a10d6274d3f724391675110f3d60781e54c024d
blockiosaurus <blockiosaurus@gmail.com>
2024-05-05 17:59:39 -0400

    Adding TODO.

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,7 @@ cfg_if::cfg_if! {
         pub use solana_program::account_info::AccountInfo;
         pub use solana_program::program_error::ProgramError;
         pub use solana_program::pubkey::Pubkey;
+    } else {
+        // TODO: Redefine types so the crate can be minimal dependency.
     }
 }
