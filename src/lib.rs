5def7dfab7a1bc07b12ebac24a02b049d3f613d5
blockiosaurus <blockiosaurus@gmail.com>
2024-05-05 16:16:18 -0400

    Initial Commit.

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+cfg_if::cfg_if! {
+    if #[cfg(feature = "legacy")]{
+        pub use solana_program::account_info::AccountInfo;
+        pub use solana_program::program_error::ProgramError;
+        pub use solana_program::pubkey::Pubkey;
+    }
+}
